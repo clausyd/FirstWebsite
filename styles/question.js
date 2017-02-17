18bbd119bc95647e10b88b0ceeffdcbf542f0c14
clausyd <clausy@hotmail.com>
2017-02-17 21:52:43 +0000

    my first website

---
+++ styles/question.js
@@ -0,0 +1,116 @@
+
+$('#choice0').click(function (){
+	
+	$('.button1').css("background-color", "Green");
+	setTimeout(function() {
+		alert('Correct, Move to Next Page');
+		$('.button1').css("background-color", "#bac9e2");
+	}, 100);
+
+} );
+
+$('#choice1').click(function(){
+	$('.button2').css("background-color", "red");
+	setTimeout(function() {
+		alert('Wrong Answer: This means, see you later');
+		$('.button2').css("background-color", "#bac9e2");
+	}, 100);
+
+	
+});
+
+$('#choice2').click(function(){
+	$('.button3').css("background-color", "red");
+	setTimeout(function() {
+		alert('Wrong Answer: This means, you are welcome');
+		$('.button3').css("background-color", "#bac9e2");
+	}, 100);
+	
+});
+
+
+
+$('#choice3').click(function(){
+	$('.button4').css("background-color", "red");
+	setTimeout(function() {
+		alert('Wrong Answer: This means, good afternoon');
+		$('.button4').css("background-color", "#bac9e2");
+	}, 100);
+});
+
+$('#choice4').click(function(){
+	$('.button5').css("background-color", "Green");
+	setTimeout(function() {
+		alert('Correct: Move to Next Page');
+		$('.button5').css("background-color", "#bac9e2");
+	}, 100);
+
+	
+});
+
+$('#choice5').click(function(){
+	$('.button6').css("background-color", "red");
+	setTimeout(function() {
+		alert('Wrong Answer: This means, good night');
+		$('.button6').css("background-color", "#bac9e2");
+	}, 100);
+	
+});
+
+
+
+$('#choice6').click(function(){
+	$('.button7').css("background-color", "red");
+	setTimeout(function() {
+		alert('Wrong Answer: This, means cheers');
+		$('.button7').css("background-color", "#bac9e2");
+	}, 100);
+});
+
+$('#choice7').click(function(){
+	$('.button8').css("background-color", "red");
+	setTimeout(function() {
+		alert('Wrong Answer: This means, I dint understand');
+		$('.button8').css("background-color", "#bac9e2");
+	}, 100);
+
+	
+});
+
+$('#choice8').click(function(){
+	$('.button9').css("background-color", "Green");
+	setTimeout(function() {
+		alert('Correct: Move to Next Page');
+		$('.button9').css("background-color", "#bac9e2");
+	}, 100);
+	
+});
+
+
+$('#choice9').click(function(){
+	$('.button10').css("background-color", "red");
+	setTimeout(function() {
+		alert('Wrong Answer: This means, What is your name');
+		$('.button10').css("background-color", "#bac9e2");
+	}, 100);
+});
+
+$('#choice10').click(function(){
+	$('.button11').css("background-color", "red");
+	setTimeout(function() {
+		alert('Wrong Answer: This means, I am fine' );
+		$('.button11').css("background-color", "#bac9e2");
+	}, 100);
+
+	
+});
+
+$('#choice11').click(function(){
+	$('.button12').css("background-color", "Green");
+	setTimeout(function() {
+		alert('Correct: Move to Next Page');
+		$('.button12').css("background-color", "#bac9e2");
+	}, 100);
+	
+});
+
